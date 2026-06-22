ea0222ac28c97771633380313d2b9f73b1e91e96
Verlif <1650825929@qq.com>
2026-06-22 09:50:13 +0000

    Deploying to gh-pages from @ Verlif/storyforge@ceb7aa9549fa47928f879f47831613eb8b6c8b0a 🚀

--- src/components/character/CharacterMainPanel.tsx
+++
@@ -1,6 +0,0 @@
-import CharacterPanel from './CharacterPanel'
-import type { Project } from '../../lib/types'
-
-export default function CharacterMainPanel({ project }: { project: Project }) {
-  return <CharacterPanel project={project} view="main" />
-}
